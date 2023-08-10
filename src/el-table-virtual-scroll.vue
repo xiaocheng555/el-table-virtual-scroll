7849509ba4e8347c9634fd9c324d1dbf11a250af
xiaocheng555 <1052745517@qq.com>
2023-08-10 13:52:36 +0800

    fix: 如果使用v-show 进行切换表格会特别卡顿 #30

--- src/el-table-virtual-scroll.vue
+++ src/el-table-virtual-scroll.vue
@@ -230,6 +230,12 @@ export default {
     // 处理滚动事件
     handleScroll (shouldUpdate = true) {
       if (!this.virtualized) return
+      // 【修复】如果使用v-show 进行切换表格会特别卡顿 #30；
+      // 【原因】v-show为false时，表格内滚动容器的高度为auto，没有滚动条限制，虚拟滚动计算渲染全部内容
+      if (this.isInnerScroll && !this.elTable.layout.viewportHeight) {
+        this.updatePosition()
+        return
+      }
 
       this.removeHoverRows()
       // 更新当前尺寸（高度）

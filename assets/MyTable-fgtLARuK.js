35839724e464f65f084fe686689b37e59bb8ffed
xiaocheng555 <1052745517@qq.com>
2024-09-16 00:45:00 +0800

    auto publish

---
+++ assets/MyTable-fgtLARuK.js
@@ -0,0 +1,2 @@
+import{n as a}from"./index-Bm6L1sKK.js";var o=function(){var e=this,t=e.$createElement,r=e._self._c||t;return r("el-table",e._b({},"el-table",e.$attrs,!1),[e._t("default")],2)},s=[];const _={name:"my-table"},n={};var c=a(_,o,s,!1,l,"2034dc36");function l(e){for(let t in n)this[t]=n[t]}const f=function(){return c.exports}();export{f as default};
+//# sourceMappingURL=MyTable-fgtLARuK.js.map
